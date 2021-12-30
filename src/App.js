a805f6ddeda909ce69ffa8880541c13de899c59d
Meredith Davis <meredithd23@yahoo.com>
2021-12-29 20:24:21 -0600

    bootstrap-icons installed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import NavbarHeader from './Components/NavbarHeader';
 import './App.css';
 import Login from './Components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './Components/Dashboard';
+import EditCard from './Components/EditCard';
 
 
 function App() {
   return (
     <div className="App">
       <NavbarHeader />
-      <Login />
+      {/* <Login /> */}
+      {/* <Dashboard /> */}
+      <EditCard />
     </div>
   );
 }
