d78e6270a12bfb36020dc980b2a37e3e18c34e79
Meredith Davis <meredithd23@yahoo.com>
2022-01-11 20:30:47 -0600

    sign up redirect

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,77 +1,84 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import './Signup.css'
+import './Signup.css';
+import { useNavigate } from 'react-router-dom';
 
-function MyVerticallyCenteredModal(props) {
-    return (
-        <div className="thankYou">
-      <Modal 
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered bg="light"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Thank you for registering!</h4>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-      </div>
-    );
-  }
+const SignUp = (props) => {
+    const [validated, setValidated] = useState(false);
+    let navigate = useNavigate();
 
+    // const navigate = useNavigate();
+    // const [error, setError] = React.useState(null);
 
-function SignUp() {
-    const [modalShow, setModalShow] = React.useState(false);
+    // async function handleClick(event) {
+    //     event.preventDefault();
+    //     let result = await submitForm(event.target);
+    //     if (result.error) {
+    //       setError(result.error);
+    //     } else {
+    //       navigate('success');
+    //     }
+    //   }
 
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+    navigate('/')
+  };
         return (
             <div className='signup'>
-                <Form>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridFirstName">
                             <Form.Label>First Name</Form.Label>
-                            <Form.Control type="firstName" placeholder="First name" />
+                            <Form.Control required type="firstName" placeholder="First name" />
+                            <Form.Control.Feedback type="invalid">
+              Please enter your first name.
+            </Form.Control.Feedback>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridLastName">
                             <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="lastName" placeholder="Last name" />
+                            <Form.Control required type="lastName" placeholder="Last name" />
+                            <Form.Control.Feedback type="invalid">
+              Please enter your last name.
+            </Form.Control.Feedback>
                         </Form.Group>
                     </Row>
 
                     <Form.Group className="mb-3" controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control required type="email" placeholder="Enter email" />
+                        <Form.Control.Feedback type="invalid">
+              Please enter your email.
+            </Form.Control.Feedback>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formGridPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control required type="password" placeholder="Password" />
+                        <Form.Control.Feedback type="invalid">
+              Please enter your password.
+            </Form.Control.Feedback>
                     </Form.Group>
                     <div class="text-center">
-                    <Button className="mt-5" variant="primary" type="register" onClick={() => setModalShow(true)}>
+                        {/* <Link class="btn btn-info" role="button">Link Button</Link> */}
+                    <Button className="mt-5" variant="primary" type="submit">
         Register
       </Button>
                     </div>
                 </Form>
-                <MyVerticallyCenteredModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
             </div>
         )
     }
-
+  
 export default SignUp
 
