232e4e0cd82163d106f06cda54a89f14b841f9c2
Michael Scoggins <rhythmghosts@gmail.com>
2022-04-05 19:49:03 -0500

    added some prettier config options. feel free to change

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,59 +1,58 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 // import data from '../data/db.json';
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import StudySet from "./StudySet";
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
+import StudySet from "./StudySet"
 
-import styles from "./Dashboard.module.css";
+import styles from "./Dashboard.module.css"
 
-const STUDYSET_API =
-  "https://flipcardzdb.herokuapp.com/cardset/all-sets-flashcards";
+const STUDYSET_API = "https://flipcardzdb.herokuapp.com/cardset/all-sets-flashcards"
 
 const FILTER_MAP = {
   All: () => true,
   Medical: (set) => set.course === "Medical",
   Dental: (set) => set.course === "Dental",
   Coding: (set) => set.course === "Coding",
-};
+}
 
-const FILTER_NAMES = Object.keys(FILTER_MAP);
+const FILTER_NAMES = Object.keys(FILTER_MAP)
 
 export default function Dashboard() {
-  const [studySets, setStudySets] = useState([]);
-  const [filter, setFilter] = useState("All");
+  const [studySets, setStudySets] = useState([])
+  const [filter, setFilter] = useState("All")
 
   useEffect(() => {
     fetch(STUDYSET_API)
       .then((res) => res.json())
       .then((data) => setStudySets(data))
-      .catch((error) => console.log(error));
-  }, []);
+      .catch((error) => console.log(error))
+  }, [])
 
   const handleDelete = async (id) => {
     await axios
       .delete(`https://flipcardzdb.herokuapp.com/cardset/${id}`)
-      .catch((e) => console.log("delete set error", e));
-    let updatedCardSets = studySets.filter((set) => set.set_id !== id);
+      .catch((e) => console.log("delete set error", e))
+    let updatedCardSets = studySets.filter((set) => set.set_id !== id)
     // let updatedStudySets = { cardSets: updatedCardSets };
-    setStudySets(updatedCardSets);
-  };
+    setStudySets(updatedCardSets)
+  }
 
   const handleUpdateStudySets = (updatedSet) => {
-    let newStudySets = [...studySets];
+    let newStudySets = [...studySets]
     let foundSetIndex = newStudySets.findIndex(
       (set) => Number(set.set_id) === Number(updatedSet.set_id)
-    );
-    newStudySets.splice(foundSetIndex, 1, updatedSet);
-    setStudySets(newStudySets);
-  };
+    )
+    newStudySets.splice(foundSetIndex, 1, updatedSet)
+    setStudySets(newStudySets)
+  }
 
-  const filterCourses = (e) => setFilter(e.target.value);
+  const filterCourses = (e) => setFilter(e.target.value)
 
   return (
     <Container>
@@ -62,10 +61,7 @@ export default function Dashboard() {
         <Form>
           <Form.Group className="mb-3">
             <Form.Label>Filter by Course</Form.Label>
-            <Form.Select
-              aria-label="Filter by Course"
-              onChange={(e) => filterCourses(e)}
-            >
+            <Form.Select aria-label="Filter by Course" onChange={(e) => filterCourses(e)}>
               {FILTER_NAMES.map((name, index) => (
                 <option key={index} value={name}>
                   {name}
@@ -93,5 +89,5 @@ export default function Dashboard() {
         </Link>
       </div>
     </Container>
-  );
+  )
 }
