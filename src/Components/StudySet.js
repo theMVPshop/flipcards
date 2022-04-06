74382dd7bc4b38667bee7d92e05a9bf9a74a4a17
Michael Scoggins <rhythmghosts@gmail.com>
2022-04-05 19:40:47 -0500

    fixed dashboard delete button

--- src/Components/StudySet.js
+++ src/Components/StudySet.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import EditCard from './EditCard';
+import EditCard from "./EditCard";
 
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-import './StudySet.css';
+import "./StudySet.css";
 
 const StudySet = ({
-    // From Dashboard
-    setInfo,
-    handleDelete,
-    handleUpdateStudySets
+  // From Dashboard
+  setInfo,
+  handleDelete,
+  handleUpdateStudySets,
 }) => {
-    return (
-        <Card className="card-study-set">
-            <Link to={`/flashcards/${setInfo.set_id}`}>
-                <Card.Body>
-                    <Card.Title>{setInfo.set_name}</Card.Title>
-                </Card.Body>
-            </Link>
-            <Card.Footer>
-                <EditCard studySetInfo={setInfo} handleUpdateStudySets={handleUpdateStudySets} />
-                <Button
-                    className="btn-delete"
-                    onClick={() => handleDelete(setInfo.id)}
-                >
-                    Delete
-                </Button>
-            </Card.Footer>
-        </Card>
-    )
-}
+  return (
+    <Card className="card-study-set">
+      <Link to={`/flashcards/${setInfo.set_id}`}>
+        <Card.Body>
+          <Card.Title>{setInfo.set_name}</Card.Title>
+        </Card.Body>
+      </Link>
+      <Card.Footer>
+        <EditCard
+          studySetInfo={setInfo}
+          handleUpdateStudySets={handleUpdateStudySets}
+        />
+        <Button
+          className="btn-delete"
+          onClick={() => handleDelete(setInfo.set_id)}
+        >
+          Delete
+        </Button>
+      </Card.Footer>
+    </Card>
+  );
+};
 
 export default StudySet;
