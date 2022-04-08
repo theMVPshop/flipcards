848cdc1560f873de277a29021a9a61466d6ce53a
Sam Dunegan <cesarcisneros9@gmail.com>
2022-04-07 20:23:14 -0500

    added image to flashcard component

--- src/Components/FlashCard.js
+++ src/Components/FlashCard.js
@@ -14,8 +14,20 @@ const FlashCard = ({ card }) => {
         <button onClick={flipCard} className="flashcard">
           <div className={`flashcardInner ${flipped ? "showBack" : "showFront"}`}>
             <div className="flashcardFront">
-              <p>Term: {card.term}</p>
-              <p className="cardFooter">Click to reveal definition</p>
+              {card.front_img && card.front_img.startsWith("http") ? (
+                <div>
+                  <p>Term: {card.term}</p>
+                  <div style={{ width: "300px", height: "200px", overflow: "hidden" }}>
+                    <img style={{ width: "100%" }} src={card.front_img} alt={card.term} />
+                  </div>
+                  <p className="cardFooter">Click to reveal definition</p>
+                </div>
+              ) : (
+                <div>
+                  <p>Term: {card.term}</p>
+                  <p className="cardFooter">Click to reveal definition</p>
+                </div>
+              )}
             </div>
             <div className="flashcardBack">
               <p>Definition: {card.definition}</p>
